bda345f2aa156dbc98c9c254ad589454b95d0cd8
kurobakaito <2136267433@qq.com>
2024-12-19 18:30:01 +0800

    feat: 2024/12/19

--- test.js
+++ test.js
@@ -1,14 +1,22 @@
-function testAsy(x){
-    return new Promise(resolve=>{setTimeout(() => {
-        resolve(x);
-      }, 3000)
-     }
-    )
- }
- async function testAwt(){    
-   let result =  await testAsy('hello world');
-   console.log(result);    // 3秒钟之后出现hello world
-   console.log('cuger')   // 3秒钟之后出现cuger
- }
- testAwt();
- console.log('cug')  //立即输出cug
+function curry(fn) {
+  let args = [];
+  return function temp(...newArgs) {
+    if(newArgs.length) {
+      args = [...args, ...newArgs]
+      return temp;
+    }else {
+      let val = fn.apply(this, args);
+      args = [];
+      return val;
+    }
+  }
+}
+
+function add(...args) {
+  return args.reduce((pre,cur) => {
+    return pre+cur
+  })
+}
+
+let curryAdd = curry(add);
+console.log(curryAdd(1)(2,3)(4,5)());
--- 面试/手写/review.js
+++ 面试/手写/review.js
@@ -1,6 +1,22 @@
-const arr = [1, 2, [1, 2, 3, [4, 5, 6]]]
-
-function flatten(arr) {
-  return arr.toString().split(',');
-}
-console.log(flatten(arr))
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    if(headA == null || headB == null) return null;
+    let pA = headA;
+    let set = new Set();
+    while(pA){
+      set.add(pA);
+      pA = pA.next;
+    }
+    let pB = headB;
+    while(pB) {
+      if(set.has(pB)) {
+        return pB
+      }
+      pB = pB.next;
+    }
+    return null;
+};
